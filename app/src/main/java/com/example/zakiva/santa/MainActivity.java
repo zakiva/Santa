57ed950ca67a953c8febcc786a57f30a1229fb0a
Zahi Akiva <zakiva@redhat.com>
2016-08-22 21:58:50 +0300

    Firebase integration

--- app/src/main/java/com/example/zakiva/santa/MainActivity.java
+++ app/src/main/java/com/example/zakiva/santa/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.zakiva.santa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = ">>>>>>>Debug: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void newButtonClicked(View view) {
+        Log.d(TAG, "newButtonClicked");
+        DatabaseReference myDatabase = FirebaseDatabase.getInstance().getReference();
+        myDatabase.child("example").setValue("WOW!");
     }
 }
