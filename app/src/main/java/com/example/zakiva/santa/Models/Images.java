1fd9e0c1b1a9055455a95e3714da006230353c56
Ariel Bl <arielb3330@gmail.com>
2016-08-30 02:28:00 +0300

    Add updated Images class

--- app/src/main/java/com/example/zakiva/santa/Models/Images.java
+++ app/src/main/java/com/example/zakiva/santa/Models/Images.java
@@ -24,44 +24,78 @@ import java.io.ByteArrayOutputStream;
  */
 public class Images {
 
-    private static LruCache<String, Bitmap> mMemoryCache;
-
     /*
-Use this method to update an image. The arguments are:
-imagename - the name of the image
-imageViewId - the integer that represents the imageView ID. The format is: [R.id.imageView]
-yourActivity - the activity that the imageView is in, The format is: [MainActivity.this]
-index - this helps the caching mechanism to search for the image. Each time you should call this method with a different index. The format is: ["1"]
-*/
-    static void updateImage(final String imageName, final int imageViewId, final Activity yourActivity, final String index){
+    Use this method to update an image. The arguments are:
+    imagename - the name of the image
+    imageViewId - the integer that represents the imageView ID. The format is: [R.id.imageView]
+    yourActivity - the activity that the imageView is in, The format is: [MainActivity.this]
+    index - this helps the caching mechanism to search for the image. Each time you should call this method with a different index. The format is: ["1"]
+    */
+    static void updateImage(final String imageName, final int imageViewId, final Activity yourActivity, final String index) {
         ImageView image = (ImageView) yourActivity.findViewById(imageViewId);
-        //final Bitmap bitmap = getBitmapFromMemCache(imageName);
         Bitmap bitmap = (Bitmap) Cache.getInstance().getLru().get(imageName);
         if (bitmap != null) {
             image.setImageBitmap(bitmap);
-            Log.d("aaaaaa: ", "RAM");
+            Log.d("Load from: ", "RAM");
         } else {
             SharedPreferences settings = yourActivity.getSharedPreferences("MY_DATA", 0);
             String Pic = settings.getString("IM1" + index, "NONE");
             String name = settings.getString("IM1 " + index + " " + imageName, "NONE");
-            if (!Pic.equals("NONE") && name.equals(imageName)){
+            if (!Pic.equals("NONE") && name.equals(imageName)) {
                 byte[] imageAsBytes = Base64.decode(Pic.getBytes(), Base64.DEFAULT);
                 Bitmap bm = BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length);
                 image.setImageBitmap(bm);
                 Cache.getInstance().getLru().put(imageName, bm);
-                Log.d("aaaaaa: ", "DISK");
-            }
-            else {
+                Log.d("Load from: ", "DISK");
+            } else {
                 downloadAndUpdateImage(imageName, imageViewId, yourActivity, index);
-                Log.d("aaaaaa: ", "WEB");
+                Log.d("Load from: ", "WEB");
             }
         }
     }
 
-    static void downloadAndUpdateImage(final String imageName, final int imageViewId, final Activity yourActivity, final String index){
+    /*
+    Use this method to update 2 images on the same time. The arguments are:
+    imagename - the name of the image
+    imageViewId - the integer that represents the imageView ID. The format is: [R.id.imageView]
+    yourActivity - the activity that the imageView is in, The format is: [MainActivity.this]
+    index - this helps the caching mechanism to search for the image. Each time you should call this method with a different index. The format is: ["1"]
+    */
+    static void update2Images(final String imageName, final int imageViewId, final String imageName2, final int imageViewId2, final Activity yourActivity, final String index, final String index2) {
+        ImageView image = (ImageView) yourActivity.findViewById(imageViewId);
+        Bitmap bitmap = (Bitmap) Cache.getInstance().getLru().get(imageName);
+        ImageView image2 = (ImageView) yourActivity.findViewById(imageViewId2);
+        Bitmap bitmap2 = (Bitmap) Cache.getInstance().getLru().get(imageName2);
+        if (bitmap != null && bitmap2 != null) {
+            image.setImageBitmap(bitmap);
+            image2.setImageBitmap(bitmap2);
+            Log.d("Load from: ", "RAM");
+        } else {
+            SharedPreferences settings = yourActivity.getSharedPreferences("MY_DATA", 0);
+            String Pic = settings.getString("IM1" + index, "NONE");
+            String name = settings.getString("IM1 " + index + " " + imageName, "NONE");
+            String Pic2 = settings.getString("IM1" + index2, "NONE");
+            String name2 = settings.getString("IM1 " + index2 + " " + imageName2, "NONE");
+            if ((!Pic.equals("NONE") && name.equals(imageName)) && (!Pic2.equals("NONE") && name2.equals(imageName2))) {
+                byte[] imageAsBytes = Base64.decode(Pic.getBytes(), Base64.DEFAULT);
+                Bitmap bm = BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length);
+                byte[] imageAsBytes2 = Base64.decode(Pic2.getBytes(), Base64.DEFAULT);
+                Bitmap bm2 = BitmapFactory.decodeByteArray(imageAsBytes2, 0, imageAsBytes2.length);
+                image.setImageBitmap(bm);
+                image2.setImageBitmap(bm2);
+                Cache.getInstance().getLru().put(imageName, bm);
+                Cache.getInstance().getLru().put(imageName2, bm2);
+                Log.d("Load from: ", "DISK");
+            } else {
+                downloadAndUpdate2Images(imageName, imageViewId, imageName2, imageViewId2, yourActivity, index, index2);
+                Log.d("Load from: ", "WEB");
+            }
+        }
+    }
+
+    static void downloadAndUpdateImage(final String imageName, final int imageViewId, final Activity yourActivity, final String index) {
         FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference storageRef = storage.getReferenceFromUrl("gs://windis-72265.appspot.com");
-
         storageRef.child(imageName).getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
             @Override
             public void onSuccess(byte[] bytes) {
@@ -69,32 +103,62 @@ index - this helps the caching mechanism to search for the image. Each time you 
                 ImageView imageView = (ImageView) yourActivity.findViewById(imageViewId);
                 imageView.setImageBitmap(bm);
                 imageToDisk(bm, imageName, yourActivity, index);
-                //addBitmapToMemoryCache(imageName, bm);
                 Cache.getInstance().getLru().put(imageName, bm);
-
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception exception) {
-                Log.d("problem: ","can't load image");
+                Log.d("problem: ", "can't load image");
             }
         });
     }
 
-    static void imageToDisk(Bitmap bm, String imageName, final Activity yourActivity, String index){
+    static void imageToDisk(Bitmap bm, String imageName, final Activity yourActivity, String index) {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         bm.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
         byte[] byteArray = byteArrayOutputStream.toByteArray();
         String pic1 = Base64.encodeToString(byteArray, Base64.DEFAULT);
-
         //save image to local database
         SharedPreferences settings = yourActivity.getSharedPreferences("MY_DATA", 0);
         SharedPreferences.Editor editor = settings.edit();
         editor.putString("IM1" + index, pic1);
         editor.putString("IM1 " + index + " " + imageName, imageName);
-        //editor.putString("PicName1", imageName);
         editor.commit();
     }
 
+    static void downloadAndUpdate2Images(final String imageName, final int imageViewId, final String imageName2, final int imageViewId2, final Activity yourActivity, final String index, final String index2) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        final StorageReference storageRef = storage.getReferenceFromUrl("gs://windis-72265.appspot.com");
+        storageRef.child(imageName).getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                final Bitmap bm = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                final ImageView imageView = (ImageView) yourActivity.findViewById(imageViewId);
+                storageRef.child(imageName2).getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes2) {
+                        Bitmap bm2 = BitmapFactory.decodeByteArray(bytes2, 0, bytes2.length);
+                        ImageView imageView2 = (ImageView) yourActivity.findViewById(imageViewId2);
+                        imageView.setImageBitmap(bm);
+                        imageView2.setImageBitmap(bm2);
+                        imageToDisk(bm, imageName, yourActivity, index);
+                        imageToDisk(bm2, imageName2, yourActivity, index2);
+                        Cache.getInstance().getLru().put(imageName, bm);
+                        Cache.getInstance().getLru().put(imageName2, bm2);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Log.d("problem: ", "can't load image");
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Log.d("problem: ", "can't load image");
+            }
+        });
+    }
 
 }
