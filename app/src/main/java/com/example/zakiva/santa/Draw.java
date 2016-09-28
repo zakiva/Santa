f83f3e1c4cff0631e0555c0471a65e01d508bef8
Zahi Akiva <zakiva@redhat.com>
2016-09-28 10:56:32 +0300

    Draw and erase

--- app/src/main/java/com/example/zakiva/santa/Draw.java
+++ app/src/main/java/com/example/zakiva/santa/Draw.java
@@ -1,30 +1,15 @@
 package com.example.zakiva.santa;
 
-import android.content.Context;
-import android.content.res.Resources;
+
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
-import com.example.zakiva.santa.Models.*;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.example.zakiva.santa.Models.MainDrawingView;
-import com.google.android.gms.ads.formats.NativeAd;
-import com.google.android.gms.vision.text.Text;
-
-
-import java.io.ByteArrayOutputStream;
+import com.example.zakiva.santa.Helpers.Drawing;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -32,234 +17,60 @@ import java.util.Collections;
 
 public class Draw extends AppCompatActivity {
 
-    public static final String TAG = ">>>>>>>Debug: ";
     public static int SIZE;
     public static int JUMP;
     public int randomImage;
     private Button pen;
     private Button eraser;
+    private View v;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw);
+        Drawing.initDrawingHelper();
+        initFields();
+    }
+
+    public void initFields () {
         SIZE = MainDrawingView.SIZE;
         JUMP = MainDrawingView.JUMP;
-        ArrayList <Integer> images = new ArrayList<Integer>(Arrays.asList(R.drawable.star700sq, R.drawable.bone700sq, R.drawable.heart700sq, R.drawable.house700sq, R.drawable.nike700sq, R.drawable.tree700sq));
-        //Collections.shuffle(images);
-        randomImage = images.get(0);
-
         pen = (Button) findViewById(R.id.pen);
         eraser = (Button) findViewById(R.id.eraser);
-
-
-        // get height and width of screen
-        //int screenWidth = displayMetrics.widthPixels;
-        //int screenHeight = displayMetrics.heightPixels;
+        v = findViewById(R.id.single_touch_view);
+        ArrayList <Integer> images = new ArrayList<Integer>(Arrays.asList(R.drawable.star700sq, R.drawable.bone700sq, R.drawable.heart700sq, R.drawable.house700sq, R.drawable.nike700sq, R.drawable.tree700sq));
+        Collections.shuffle(images);
+        randomImage = images.get(0);
     }
 
     public void printPixelsClicked(View view) {
-        View v = ((View) findViewById(R.id.single_touch_view));
-      //  ((MainDrawingView) v).printPixels();
-        ((MainDrawingView) v).printMatrix();
-
+        //do nothing for now
     }
 
-    public void convertImageToBitampClicked(View view) {
-        Bitmap bitmap = convertImageToBitmap(randomImage);
-        drawBitmap(bitmap);
-    }
-
-    public Bitmap convertImageToBitmap(int image) {
-
-        String line;
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inScaled = false;
-        Bitmap icon = BitmapFactory.decodeResource(this.getResources(), image, options);
-
-        int h = icon.getHeight();
-        int w = icon.getWidth();
-
-        /*
-
-        for (int i = 0; i < h; i += JUMP){
-            line = "";
-            for (int j = 0; j < w; j += JUMP){
-
-              // Log.d(MainActivity.TAG, "i,j" + i + ","+j +"  = " + icon.getPixel(j,i));
-
-                if(icon.getPixel(j,i) < -1) {
-
-                    line = line + "*";
-                }
-                else {
-                    line = line + " ";
-                }
-            }
-            Log.d(MainActivity.TAG, line);
-        }
-        */
-
-       // Log.d(MainActivity.TAG, getBytesFromBitmap(icon).toString());
-
-        return icon;
-    }
-
-    public void drawBitmap (Bitmap bitmap) {
-
-        int JUMP_FOR_DRAW = 1;
-
-        int h = bitmap.getHeight();
-        int w = bitmap.getWidth();
-
-        Log.d(MainActivity.TAG, "from drawBitmap h = " + h);
-        Log.d(MainActivity.TAG, "from drawBitmap w = " + w);
-
-        // Create a bitmap of the same size
-        Bitmap newBmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-// Create a canvas  for new bitmap
-        Canvas c = new Canvas(newBmp);
-
-// Draw your old bitmap on it.
-        c.drawBitmap(bitmap, 0, 0, new Paint());
-
-        for (int i = 0; i < h; i += JUMP_FOR_DRAW){
-            for (int j = 0; j < w; j += JUMP_FOR_DRAW){
-
-                newBmp.setPixel(j,i,bitmap.getPixel(j,i));
-
-                //if you want ot cut the image set any condition here:
-
-                /*
-
-                if(i>300) {
-                    newBmp.setPixel(j,i,-1);
-                }
-                else {
-                    newBmp.setPixel(j,i,bitmap.getPixel(j,i));
-                }
-
-                */
-            }
-        }
-
-
-        Drawable d = new BitmapDrawable(getResources(), newBmp);
-        //ImageView v = ((ImageView) findViewById(R.id.imageView));
-        View v = ((View) findViewById(R.id.single_touch_view));
-        v.setBackground(d);
-    }
-
-    public byte[] getBytesFromBitmap(Bitmap bitmap) {
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
-        return stream.toByteArray();
-    }
-
-    /**
-     * This method converts dp unit to equivalent pixels, depending on device density.
-     *
-     * @param dp A value in dp (density independent pixels) unit. Which we need to convert into pixels
-     * @param context Context to get resources and device specific display metrics
-     * @return A float value to represent px equivalent to dp depending on device density
-     */
-    public static float convertDpToPixel(float dp, Context context){
-        Resources resources = context.getResources();
-        DisplayMetrics metrics = resources.getDisplayMetrics();
-        float px = dp * ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
-        return px;
-    }
-
-    /**
-     * This method converts device specific pixels to density independent pixels.
-     *
-     * @param px A value in px (pixels) unit. Which we need to convert into db
-     * @param context Context to get resources and device specific display metrics
-     * @return A float value to represent dp equivalent to px value
-     */
-    public static float convertPixelsToDp(float px, Context context){
-        Resources resources = context.getResources();
-        DisplayMetrics metrics = resources.getDisplayMetrics();
-        float dp = px / ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
-        return dp;
-    }
-
-    public int [][] convertBitmapToMatrix (Bitmap bitmap) {
-
-        int [][] matrix = new int[SIZE][SIZE];
-        int h = bitmap.getHeight();
-        int w = bitmap.getWidth();
-
-        Log.d(MainActivity.TAG, "from convertBitmapToMatrix h = " + h);
-        Log.d(MainActivity.TAG, "from convertBitmapToMatrix w = " + w);
-
-        for (int i = 0; i < h; i += JUMP){
-            for (int j = 0; j < w; j += JUMP){
-
-                if(bitmap.getPixel(j,i) < -1) {
-                    matrix[i][j] = 1;
-
-                }
-                else {
-                    //default valus is 0
-               }
-            }
-        }
-        return matrix;
+    public void convertImageToBitmapClicked(View view) {
+        Bitmap bitmap = Drawing.convertImageToBitmap(randomImage, this);
+        Drawing.drawBitmap(bitmap, this, v);
     }
 
     public void compareImagesClicked(View view) {
 
-        Bitmap source_bitmap = convertImageToBitmap(randomImage);
-        int [][] source_matrix = convertBitmapToMatrix(source_bitmap);
-        printMatrix(source_matrix);
+        Bitmap draw_bitmap = ((MainDrawingView) v).canvasBitmap;
+        Bitmap source_bitmap = Drawing.convertImageToBitmap(randomImage, this);
 
-        View v = ((View) findViewById(R.id.single_touch_view));
-        int [] result = ((MainDrawingView) v).compareMatrices(source_matrix);
+        int [][] source_matrix = Drawing.convertBitmapToMatrix(source_bitmap);
+        int [][] user_matrix = Drawing.convertBitmapToMatrix(draw_bitmap);
 
-        TextView g = ((TextView) findViewById(R.id.goodPoints));
-        TextView b = ((TextView) findViewById(R.id.badPoints));
-        g.setText("Good Points: " + result[0]);
-        b.setText("Bad Points: " + result[1]);
-    }
+        Drawing.printMatrix(source_matrix);
 
-    public static void printMatrixBits(int [][] matrix) {
-        String line;
-        for (int i = 0; i < SIZE; i += 1){
-            line = "";
-            for (int j = 0; j < SIZE; j += 1){
-                if(matrix[i][j] > 0) {
-                    line = line + "1";
-                }
-                else {
-                    line = line + "0";
-                }
-            }
-            Log.d(MainActivity.TAG, line);
-        }
-    }
+        int [] result = Drawing.compareMatrices(source_matrix, user_matrix);
 
-
-
-    public static void printMatrix(int [][] matrix) {
-        int JUMP_PRINT = 5;
-        String line;
-        for (int i = 0; i < SIZE; i += JUMP_PRINT){
-            line = "";
-            for (int j = 0; j < SIZE; j += JUMP_PRINT){
-                if(matrix[i][j] > 0) {
-                    line = line + "*";
-                }
-                else {
-                    line = line + " ";
-                }
-            }
-            Log.d(MainActivity.TAG, line);
-        }
+        TextView good = ((TextView) findViewById(R.id.goodPoints));
+        TextView bad = ((TextView) findViewById(R.id.badPoints));
+        good.setText("Good Points: " + result[0]);
+        bad.setText("Bad Points: " + result[1]);
     }
 
     public void hideClicked(View view) {
-        View v = ((View) findViewById(R.id.single_touch_view));
         v.setBackgroundColor(Color.WHITE);
     }
 
@@ -273,7 +84,14 @@ public class Draw extends AppCompatActivity {
             pen.setBackgroundResource(R.drawable.pen_icon_color);
             eraser.setBackgroundResource(R.drawable.eraser_icon);
         }
-        View v = ((View) findViewById(R.id.single_touch_view));
         ((MainDrawingView) v).setDrawingMode(mode);
     }
+
+    public void bitmapBbuttonClicked(View view) {
+        Bitmap b = ((MainDrawingView) v).canvasBitmap;
+        int [][] m = Drawing.convertBitmapToMatrix(b);
+        Drawing.printMatrix(m);
+    }
+
+
 }
--- app/src/main/java/com/example/zakiva/santa/Models/MainDrawingView.java
+++ app/src/main/java/com/example/zakiva/santa/Models/MainDrawingView.java
@@ -7,15 +7,11 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import com.example.zakiva.santa.*;
-
 
 import com.example.zakiva.santa.MainActivity;
 
@@ -25,11 +21,10 @@ import com.example.zakiva.santa.MainActivity;
 
 public class MainDrawingView extends View {
 
-    public static final String TAG = ">>>>>>>Debug: ";
     private Paint paint;
     private Path path;
-    private Bitmap bitmap;
-    private int [][] matrix;
+    //private Bitmap bitmap;
+    //private int [][] matrix;
     public static int SIZE = 700;
     public static int JUMP = 5;
     private static final float RATIO = 1f / 1f;
@@ -39,11 +34,9 @@ public class MainDrawingView extends View {
     //canvas
     private Canvas drawCanvas;
     //canvas bitmap
-    private Bitmap canvasBitmap;
+    public Bitmap canvasBitmap;
     //canvas paint
     private Paint canvasPaint;
-
-
 
     public MainDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -53,7 +46,7 @@ public class MainDrawingView extends View {
         path = new Path();
 
         paint.setAntiAlias(true);
-        paint.setStrokeWidth(11f);
+        paint.setStrokeWidth(5f);
         paint.setColor(Color.BLACK);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeJoin(Paint.Join.ROUND);
@@ -62,17 +55,17 @@ public class MainDrawingView extends View {
 
         Resources resources = context.getResources();
         DisplayMetrics metrics = resources.getDisplayMetrics();
-        int screenWidth = metrics.widthPixels;
-        int screenHeight = metrics.heightPixels;
+        //int screenWidth = metrics.widthPixels;
+        //int screenHeight = metrics.heightPixels;
         //SIZE = screenWidth;
 
         drawingMode = 1;
-        matrix = new int[SIZE][SIZE];
+        //matrix = new int[SIZE][SIZE];
 
         canvasPaint = new Paint(Paint.DITHER_FLAG);
         canvasBitmap = Bitmap.createBitmap(SIZE, SIZE, Bitmap.Config.ARGB_8888);
         drawCanvas = new Canvas(canvasBitmap);
-   }
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -100,7 +93,7 @@ public class MainDrawingView extends View {
             return false;
 
         //matrix[yRounded][xRounded] = drawingMode; // set pixel to 1 or 0
-        updateMatrixWithDelta(yRounded, xRounded);
+        //updateMatrixWithDelta(yRounded, xRounded);
 
         Log.d(MainActivity.TAG, "touched: " + x +"," + y);
 
@@ -130,131 +123,19 @@ public class MainDrawingView extends View {
         return (n + JUMP - 1) / JUMP * JUMP;
     }
 
-    public void printMatrix() {
-        Draw.printMatrix(matrix);
-    }
-
-    public int [] compareMatrices(int [][] source) {
-
-        int white_source = 0, white_matrix = 0, black_source = 0, black_matrix = 0, equal = 0, diff = 0;
-        int equal_delta = 0, bad_black_delta = 0;
-        int DELTA = 5;
-
-        for (int i = 0; i < SIZE; i += JUMP){
-            for (int j = 0; j < SIZE; j += JUMP){
-                if(matrix[i][j] > 0) {
-                    black_matrix++;
-                }
-                else {
-                    white_matrix++;
-                }
-                if(source[i][j] > 0) {
-                    black_source++;
-                }
-                else {
-                    white_source++;
-                }
-                if(matrix[i][j] == source[i][j] && matrix[i][j] == 1) {
-                    equal++;
-                }
-                else {
-                    diff++;
-                }
-                if (matrix[i][j] == 1) {
-                    if (compareWithDelta(source, i, j, DELTA)) {
-                        equal_delta++;
-                    } else {
-                        bad_black_delta++;
-                    }
-                }
-
-            }
+    public void setDrawingMode (int mode) {
+        this.drawingMode = mode;
+        if (drawingMode == 0) {
+            paint.setStrokeWidth(20f);
+            paint.setColor(Color.WHITE);
+            //paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
         }
-        Log.d(MainActivity.TAG, ">>>>>>>>>>Comparing summary<<<<<<<<<<<");
-        Log.d(MainActivity.TAG, "white source = " + white_source);
-        Log.d(MainActivity.TAG, "white matrix = " + white_matrix);
-        Log.d(MainActivity.TAG, "black source = " + black_source);
-        Log.d(MainActivity.TAG, "black matrix = " + black_matrix);
-        Log.d(MainActivity.TAG, "equal = " + equal);
-        Log.d(MainActivity.TAG, "equal delta = " + equal_delta);
-        Log.d(MainActivity.TAG, "bad black delta = " + bad_black_delta);
-        Log.d(MainActivity.TAG, "diff = " + diff);
-
-        int [] a = new int [2];
-        a[0] = equal_delta;
-        a[1] = bad_black_delta;
-        return a;
-    }
-
-    public boolean compareWithDelta (int [][] source, int i, int j, int DELTA) {
-
-        int limit = DELTA*JUMP;
-
-        if (i-limit < 0 || j-limit < 0 || i + limit > SIZE || j + limit > SIZE )
-            return matrix[i][j] == source[i][j] && matrix[i][j] == 1;
-
-        for (int k = -1 * (DELTA - 1); k < DELTA; k++) {
-            for (int l = -1 * (DELTA - 1); l < DELTA; l++) {
-                if (source[i+k*JUMP][j+l*JUMP] == 1){ // hit !!! init env.
-                    for (int m = -1 * (DELTA - 2); m < DELTA - 1; m++) {
-                        for (int n = -1 * (DELTA - 2); n < DELTA - 1; n++) {
-                            source[i+n*JUMP][j+m*JUMP] = 0;
-                        }
-                    }
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public void updateMatrixWithDelta (int i, int j) {
-
-        int DELTA = 3;
-        int limit = DELTA*JUMP;
-
-        if (i-limit < 0 || j-limit < 0 || i + limit > SIZE || j + limit > SIZE || drawingMode == 1) {
-            matrix[i][j] = drawingMode;
-            return;
-        }
-
-        for (int k = -1 * (DELTA - 1); k < DELTA; k++) {
-            for (int l = -1 * (DELTA - 1); l < DELTA; l++) {
-                matrix[i + k * JUMP][j + l * JUMP] = 0;
-            }
+        else {
+            paint.setStrokeWidth(5f);
+            paint.setColor(Color.BLACK);
+            //paint.setXfermode(null);
         }
     }
-
-
-
-    /*
-    public void printPixels () {
-        Log.d(MainActivity.TAG, "10,10 = " + bitmap.getPixel(10,10));
-        Log.d(MainActivity.TAG, "20,20 = " + bitmap.getPixel(20,20));
-        Log.d(MainActivity.TAG, "30,30 = " + bitmap.getPixel(30,30));
-        Log.d(MainActivity.TAG, "40,40 = " + bitmap.getPixel(40,40));
-        Log.d(MainActivity.TAG, "50,50 = " + bitmap.getPixel(50,50));
-        Log.d(MainActivity.TAG, "60,60 = " + bitmap.getPixel(60,60));
-
-        for (int i =0; i<10; i++){
-            for (int j=0; j<10; j++){
-                Log.d(MainActivity.TAG, "(400,400), i = " + i + ", j = " + j + ", pixel:" + bitmap.getPixel(400 + i ,400 + j));
-            }
-        }
-
-
-        for (int i = 0; i < 500; i++){
-            for (int j = 0; j < 500; j++){
-                if(bitmap.getPixel(i,j) > 0) {
-                    Log.d(MainActivity.TAG, "x,y = " + i + "," +  j);
-                    Log.d(MainActivity.TAG, "pixel = " + bitmap.getPixel(i,j));
-                }
-            }
-        }
-     } */
-
-
-
 
 
     @Override
@@ -294,22 +175,9 @@ public class MainDrawingView extends View {
         Log.d(MainActivity.TAG, "222 width, height = " + width + " " + height);
 
         setMeasuredDimension(width, height);
-    }
 
-    public void setDrawingMode (int mode) {
-        this.drawingMode = mode;
-        Log.d(MainActivity.TAG, "this.drawingMode = " + this.drawingMode);
-
-        if (drawingMode == 0) {
-            paint.setStrokeWidth(20f);
-            paint.setColor(Color.WHITE);
-            //paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-        }
-        else {
-            paint.setStrokeWidth(11f);
-            paint.setColor(Color.BLACK);
-            //paint.setXfermode(null);
-        }
-
+        // get height and width of screen
+        //int screenWidth = displayMetrics.widthPixels;
+        //int screenHeight = displayMetrics.heightPixels;
     }
 }
