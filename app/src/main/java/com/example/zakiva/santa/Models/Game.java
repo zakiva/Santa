6206986b5d105427ed3bfff8991f33ce25da3c49
Zahi Akiva <zakiva@redhat.com>
2016-10-20 13:37:19 +0300

    Add TimeStamp to Game

--- app/src/main/java/com/example/zakiva/santa/Models/Game.java
+++ app/src/main/java/com/example/zakiva/santa/Models/Game.java
@@ -1,5 +1,8 @@
 package com.example.zakiva.santa.Models;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 /**
  * Created by zakiva on 10/20/16.
  */
@@ -8,12 +11,19 @@ public class Game {
 
     public String type;
     public long score;
+    public long timeStamp;
+    public String dateAndTime;
 
     public Game() {}
 
     public Game (String type, long score) {
         this.type = type;
         this.score = score;
+        long currentTime = System.currentTimeMillis();
+        this.timeStamp = currentTime/1000;
+        Timestamp stamp = new Timestamp(currentTime);
+        Date date = new Date(stamp.getTime());
+        this.dateAndTime = date.toString();
     }
 
     public String getType() {
@@ -30,6 +40,22 @@ public class Game {
 
     public void setScore (long score) {
         this.score = score;
+    }
+
+    public long getTimeStamp () {
+        return timeStamp;
+    }
+
+    public void setTimeStamp (long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public void setDateAndTime(String dateAndTime) {
+        this.dateAndTime = dateAndTime;
     }
 }
 
