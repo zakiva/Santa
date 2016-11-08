6ce6e835583576bfa672f16c1464ab7d54dcf93a
Ariel Bl <arielb3330@gmail.com>
2016-11-09 01:13:18 +0200

    Add choose game slider

---
+++ app/src/main/java/com/example/zakiva/santa/Helpers/CustomPagerAdapter.java
@@ -0,0 +1,77 @@
+package com.example.zakiva.santa.Helpers;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.zakiva.santa.R;
+
+/**
+ * Created by Ariel on 11/13/2016.
+ */
+
+public class CustomPagerAdapter extends PagerAdapter {
+
+    private Context mContext;
+
+    public CustomPagerAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup collection, int position) {
+        CustomPagerEnum customPagerEnum = CustomPagerEnum.values()[position];
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        ViewGroup layout = (ViewGroup) inflater.inflate(customPagerEnum.getLayoutResId(), collection, false);
+        collection.addView(layout);
+        return layout;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup collection, int position, Object view) {
+        collection.removeView((View) view);
+    }
+
+    @Override
+    public int getCount() {
+        return CustomPagerEnum.values().length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        CustomPagerEnum customPagerEnum = CustomPagerEnum.values()[position];
+        return mContext.getString(customPagerEnum.getTitleResId());
+    }
+
+    public enum CustomPagerEnum {
+
+        gameButton1(1, R.layout.gamebutton1),
+        gameButton2(2, R.layout.gamebutton2),
+        gameButton3(3, R.layout.gamebutton3),
+        gameButton4(4, R.layout.gamebutton4),
+        gameButton5(5, R.layout.gamebutton5);
+
+        private int mTitleResId;
+        private int mLayoutResId;
+
+        CustomPagerEnum(int titleResId, int layoutResId) {
+            mTitleResId = titleResId;
+            mLayoutResId = layoutResId;
+        }
+
+        public int getTitleResId() {
+            return mTitleResId;
+        }
+
+        public int getLayoutResId() {
+            return mLayoutResId;
+        }
+    }
+
+}
