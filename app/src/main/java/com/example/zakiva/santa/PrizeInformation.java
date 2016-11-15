2f1f73cee4689a8fe2f6aa2af383d4ef2832e13a
Ariel Bl <arielb3330@gmail.com>
2016-11-16 01:31:26 +0200

    Add example image

--- app/src/main/java/com/example/zakiva/santa/PrizeInformation.java
+++ app/src/main/java/com/example/zakiva/santa/PrizeInformation.java
@@ -3,11 +3,16 @@ package com.example.zakiva.santa;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.zakiva.santa.Models.Images;
+
 public class PrizeInformation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prize_information);
+
+        //Set image from server
+        Images.updateImage("add_text.jpeg", R.id.imageViewExample,PrizeInformation.this, "1");
     }
 }
