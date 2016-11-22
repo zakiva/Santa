cfb0afb2a8d3ce67e64868a05210807a5784efd6
Micha <bmicha6@gmail.com>
2016-11-22 02:49:42 +0200

    adding design to information prize activity

--- app/src/main/java/com/example/zakiva/santa/PrizeInformation.java
+++ app/src/main/java/com/example/zakiva/santa/PrizeInformation.java
@@ -2,17 +2,35 @@ package com.example.zakiva.santa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 import com.example.zakiva.santa.Models.Images;
 
 public class PrizeInformation extends AppCompatActivity {
 
+    private TabHost tabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prize_information);
 
+        tabHost = (TabHost) findViewById(R.id.tabsBox);
+        createTabs();
         //Set image from server
         Images.updateImage("add_text.jpeg", R.id.imageViewExample,PrizeInformation.this, "1");
+    }
+
+    public void createTabs(){
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("fashion");
+        tabSpec.setContent(R.id.fashionTab);
+        tabSpec.setIndicator("Fashion");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("food");
+        tabSpec.setContent(R.id.foodTab);
+        tabSpec.setIndicator("Food");
+        tabHost.addTab(tabSpec);
     }
 }
